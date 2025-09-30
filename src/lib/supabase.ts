da0c21e857ed4b4b2de85fab2ec847563e0b7146
rimu90s <ridwanmuchlis33@gmail.com>
2025-09-30 17:23:03 +0700

    adjust supabase.ts dengan adanya tambahan env.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { env } from "./env";
 
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    env.NEXT_PUBLIC_SUPABASE_URL!,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
